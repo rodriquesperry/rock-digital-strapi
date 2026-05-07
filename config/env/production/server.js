7b53db20b4666cc5965d7d4e3e4a37e108df5f6e
Rodriques <rodriques.perry@gmail.com>
2026-05-07 16:06:48 -0500

    Update proxy config/env/production/server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  proxy: true,
+  proxy: {
+    koa: true,
+  },
   url: env("PUBLIC_URL", "https://rockdigital.agency"),
   app: {
     keys: env.array("APP_KEYS"),
