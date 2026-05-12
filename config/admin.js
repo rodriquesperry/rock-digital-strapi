d8b28850bf9f030fdb03eaa58e420454a4924b73
Rodriques <rodriques.perry@gmail.com>
2026-05-12 12:47:47 -0500

    Update: add Strapi preview

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,55 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env("ADMIN_JWT_SECRET"),
-  },
-  apiToken: {
-    salt: env("API_TOKEN_SALT"),
-  },
-  transfer: {
-    token: {
-      salt: env("TRANSFER_TOKEN_SALT"),
+const getPreviewPathname = (uid, document) => {
+  if (uid !== "api::post.post") {
+    return null;
+  }
+
+  if (!document?.slug) {
+    return "/blog";
+  }
+
+  return `/blog/${document.slug}`;
+};
+
+module.exports = ({ env }) => {
+  const clientUrl = env("CLIENT_URL", "http://localhost:3000").replace(/\/$/, "");
+  const previewSecret = env("PREVIEW_SECRET");
+
+  return {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
     },
-  },
-});
+    apiToken: {
+      salt: env("API_TOKEN_SALT"),
+    },
+    transfer: {
+      token: {
+        salt: env("TRANSFER_TOKEN_SALT"),
+      },
+    },
+    preview: {
+      enabled: true,
+      config: {
+        allowedOrigins: clientUrl,
+        async handler(uid, { documentId, status }) {
+          const document = await strapi.documents(uid).findOne({
+            documentId,
+            status: status === "published" ? "published" : "draft",
+          });
+          const pathname = getPreviewPathname(uid, document);
+
+          if (!pathname) {
+            return null;
+          }
+
+          const urlSearchParams = new URLSearchParams({
+            url: pathname,
+            secret: previewSecret,
+            status,
+          });
+
+          return `${clientUrl}/api/preview?${urlSearchParams}`;
+        },
+      },
+    },
+  };
+};
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // proxy: {
-  //   koa: true,
-  // },
+  proxy: {
+    koa: true,
+  },
   url: env("PUBLIC_URL", "https://rockdigital.agency"),
   app: {
     keys: env.array("APP_KEYS"),
